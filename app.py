f477630a5b074335cd0392107557147c1fb88c8c
Vedant Raut <105361526+vedant185raut@users.noreply.github.com>
2025-01-09 14:55:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask 
-app = Flask(__name__) 
- 
-@app.route("/") 
-def hello_world(): 
-    return "Hello, World from Python by Vedant Raut!" 
- 
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return "Hello, World from Python on Azure!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
